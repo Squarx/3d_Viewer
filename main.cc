0a6f25bf981ceed5a61a3bb97b40737ad8f716af
gulianbo <dimasclashx@gmail.com>
2023-08-09 23:13:05 +0300

    pharse f func

--- main.cc
+++ main.cc
@@ -5,7 +5,102 @@
 #include <string>
 #include <vector>
 
-bool ProcessVertex(std::vector<double> &vertex_, std::string line) {
+class ObjReader {
+ public:
+  ObjReader() = default;
+  ObjReader(std::string file_path) : file_path_(file_path) {}
+  void PrintFacets();
+  void PrintVertex();
+  int ParseFile();
+
+ private:
+  std::vector<double> vertex_{};
+  std::vector<unsigned int> facets_{};
+  std::string file_path_;
+  bool ProcessVertex(std::string line);
+  bool ProcessFacets(std::string line);
+};
+
+int ObjReader::ParseFile() {
+  std::string line{};
+  std::ifstream in(file_path_);
+
+  if (in.is_open()) {
+    while (std::getline(in, line)) {
+      if (!ProcessVertex(line) && !ProcessFacets(line)) {
+        continue;
+      }
+    }
+
+    in.close();
+  } else {
+    std::cerr << "Unable to open file cube.obj" << std::endl;
+    return 1;
+  }
+  return 0;
+}
+
+void ObjReader::PrintVertex() {
+  printf("===================VERTEX===================\n");
+  for (auto i = 0; i < vertex_.size(); ++i) {
+    if (i % 3 == 0) {
+      std::cout << std::endl;
+    }
+    printf("%-10f ", vertex_[i]);
+  }
+  std::cout << std::endl;
+
+  printf("=============================================\n");
+}
+
+void ObjReader::PrintFacets() {
+  printf("===================FACETS===================\n");
+  for (auto i = 0; i < facets_.size(); ++i) {
+    if (i % 6 == 0) {
+      std::cout << std::endl;
+    }
+    printf("%-4u ", facets_[i]);
+  }
+  std::cout << std::endl;
+  printf("=============================================\n");
+}
+
+bool ObjReader::ProcessFacets(std::string line) {
+  if (line.compare(0, 2, "f ") == 0) {
+    std::string shifted_line = line.substr(2);
+    std::istringstream iss(shifted_line);
+    std::string token;
+    bool first_iteration = true;
+    int first_id{};
+    int prev_id{};
+
+    while (getline(iss, token, ' ')) {
+      int a = std::stoi(token);
+      a -= 1;
+
+      if (a < 0) {
+        a += vertex_.size() + 1;
+      }
+
+      if (first_iteration) {
+        first_id = a;
+        first_iteration = false;
+      } else {
+        facets_.push_back(prev_id);
+        facets_.push_back(a);
+      }
+
+      prev_id = a;
+    }
+
+    facets_.push_back(prev_id);
+    facets_.push_back(first_id);
+    return true;
+  }
+  return false;
+}
+
+bool ObjReader::ProcessVertex(std::string line) {
   if (line[0] == 'v' && line[1] == ' ') {
     std::string shifted_line = line.substr(2);
     std::istringstream iss(shifted_line);
@@ -17,119 +112,12 @@ bool ProcessVertex(std::vector<double> &vertex_, std::string line) {
   return false;
 }
 
-
-bool ProcessFacets(std::vector<unsigned int> &facets_, std::string line) {
-    if (line.compare(0, 2, "f ") == 0) {
-        std::cout << line << "\n";
-        std::string shifted_line = line.substr(2);
-        std::istringstream iss(shifted_line);
-        std::string token;
-        bool first_iteration = true;
-        int first_id{};
-        int prev_id{};
-
-        while (getline(iss, token, ' ')) {
-            int a = std::stoi(token);
-            a-=1;
-            //        if (vertex_index < 0) {
-            //            vertex_index += vertexes_.size() + 1;
-            //        }
-            if (first_iteration) {
-                first_id = a;
-                first_iteration = false;
-            } else {
-                facets_.push_back(prev_id);
-                facets_.push_back(a);
-            }
-
-            prev_id = a;
-            std::cout << a << std::endl;
-        }
-
-        facets_.push_back(prev_id);
-        facets_.push_back(first_id);
-        return true;
-    }
-    return false;
-}
-
-void ReadFacet(const std::string &line) {
-  std::istringstream iss(line);
-  iss.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
-  std::vector<unsigned int> facets_{};
-  int vertex_index{}, prev_vertex_index{}, first_vertex_index{};
-  bool first_iteration = true;
-
-  while (iss >> vertex_index) {
-    vertex_index -= 1;
-    //        if (vertex_index < 0) {
-    //            vertex_index += vertexes_.size() + 1;
-    //        }
-    if (!first_iteration) {
-      facets_.push_back(prev_vertex_index);
-      facets_.push_back(vertex_index);
-    } else {
-      first_vertex_index = vertex_index;
-      first_iteration = false;
-    }
-    prev_vertex_index = vertex_index;
-
-    iss.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
-  }
-  facets_.push_back(prev_vertex_index);
-  facets_.push_back(first_vertex_index);
-
-  for (auto i : facets_) {
-    std::cout << i << std::endl;
-  }
-}
-
-// int main() {
-// std::string s{"f 1 4 2"};
-//     ReadFacet(s);
-//
-// }
-
-
-
 int main() {
-  std::vector<double> vertex_{};
-  std::vector<unsigned int> facets_{};
-  std::string line;
-  std::ifstream in("cube.obj");
-  //^v(\s(-?(0|([1-9]\d*))(\.\d+)?)){3}$
+  std::string line{"cube.obj"};
+  ObjReader a{line};
+  a.ParseFile();
+  a.PrintVertex();
+  a.PrintFacets();
 
-  if (in.is_open()) {
-    std::regex v_pattern(R"(^v(\s(-?(0|([1-9]\d*))(\.\d+)?)){3})");
-
-    while (std::getline(in, line)) {
-      if (!ProcessVertex(vertex_, line) && !ProcessFacets(facets_,line)) {
-          continue;
-      }
-    }
-
-    in.close();
-  } else {
-    std::cerr << "Unable to open file cube.obj" << std::endl;
-    return 1;
-  }
-
-  printf("===================VERTEX===================\n");
-  for (auto i = 0; i < vertex_.size(); ++i) {
-    if (i % 3 == 0) {
-      std::cout << std::endl;
-    }
-    printf("%-10f ", vertex_[i]);
-  }
-  printf("=============================================\n");
-
-  printf("===================FACETS===================\n");
-  for (auto i = 0; i < facets_.size(); ++i) {
-    if (i % 6 == 0) {
-      std::cout << std::endl;
-    }
-    printf("%-4u ", facets_[i]);
-  }
-  printf("=============================================\n");
   return 0;
 }
